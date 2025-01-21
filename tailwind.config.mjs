8877078244add43db17d4410099d5ca677738698
HEALTH / HEALTH <healthhealth@icloud.com>
2025-01-21 07:20:27 -0300

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,10 +8,11 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-        serif: ['Inter', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'], // Substitua 'Inter' pela fonte serifada desejada, se não for a Inter
+        serif: ['serif'],
       },
       backgroundImage: {
-        'quadriculado': " <img src="https://i1.wp.com/multarte.com.br/wp-content/uploads/2019/02/fundo-quadriculado-png-transparente4.png?resize=768%2C576&ssl=1">", // Substitua pelo caminho da imagem do quadriculado
+        'quadriculado': "url('/quadriculado.png')", // Caminho correto para a imagem na pasta public
+      },
       },
     },
   },
