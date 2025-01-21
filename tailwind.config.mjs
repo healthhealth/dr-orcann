ff1b76fdca77ab36d71a5620266cf5f038ad4ee7
HEALTH / HEALTH <healthhealth@icloud.com>
2025-01-21 06:42:48 -0300

    Atualizar o tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,10 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-        serif: ['var(--font-inter)'], // Substitua 'var(--font-inter)' pela fonte serif que desejar, ou adicione uma nova
+        serif: ['Inter', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'], // Substitua 'Inter' pela fonte serifada desejada, se não for a Inter
+      },
+      backgroundImage: {
+        'quadriculado': "url('/fundo-quadriculado.png')", // Substitua pelo caminho da imagem do quadriculado
       },
     },
   },
