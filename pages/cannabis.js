10c0c5f097a44592a25e65e1fbae4f0a67d752e8
HEALTH / HEALTH <healthhealth@icloud.com>
2025-01-21 04:53:46 -0300

    Update again

---
+++ pages/cannabis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CannabisSlides from '@/components/CannabisSlides'; // Ajuste o caminho se necessário
+
+const CannabisPage = () => {
+  return (
+    <div>
+      <CannabisSlides />
+    </div>
+  );
+};
+
+export default CannabisPage;
